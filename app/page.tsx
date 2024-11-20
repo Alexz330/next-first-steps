51a98dcc03fb40dfc3946ed188b9ad60c6d87377
alexz330 <alexislc330@gmail.com>
2024-11-20 16:59:40 -0600

    Second commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <div className="flex flex-col items-center p-24">
-      <span className="text-5xl">Hola mundo
+      <span className="text-5xl">Hola mundo - Steven Hueco
       </span>
     </div>
   );
